8783eb01cd54049b65f2a945add46a0e668d658a
Paulo Silva <paulowrsilva@gmail.com>
2024-03-13 11:08:20 -0300

    feat: add initial header component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Header from "./_components/header";
 
 export default function Home() {
   return (
     <main>
-      <h1>fsw</h1>
+      <Header />
     </main>
   );
 }
