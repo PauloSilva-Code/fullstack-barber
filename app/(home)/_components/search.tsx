14c13e5e0a13e916a3ab13cb4a3760d6971cf876
Paulo Silva <paulowrsilva@gmail.com>
2024-03-14 10:16:58 -0300

    feat: add initial Search component

--- app/(home)/_components/search.tsx
+++ app/(home)/_components/search.tsx
@@ -1,13 +1,15 @@
+"use client";
+
 import { Button } from "@/app/components/ui/button";
 import { Input } from "@/app/components/ui/input";
 import { SearchIcon } from "lucide-react";
 
 const Search = () => {
   return (
-    <div>
-      <Input />
-      <Button>
-        <SearchIcon />
+    <div className="flex items-center gap-2">
+      <Input placeholder="Busque por uma barbearia..." />
+      <Button variant="default">
+        <SearchIcon size={20} />
       </Button>
     </div>
   );
