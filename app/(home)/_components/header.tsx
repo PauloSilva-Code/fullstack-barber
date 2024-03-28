df28cac71c657dd5a0d66df1694dde9f01f7bdac
Paulo Silva <paulowrsilva@gmail.com>
2024-03-28 00:00:47 -0300

    feat: add booking page

--- app/(home)/_components/header.tsx
+++ app/(home)/_components/header.tsx
@@ -2,16 +2,19 @@
 
 import { Button } from "@/app/components/ui/button";
 import { Card, CardContent } from "@/app/components/ui/card";
+import { Sheet, SheetContent, SheetTrigger } from "@/app/components/ui/sheet";
 import { MenuIcon } from "lucide-react";
 import Image from "next/image";
-import { Sheet, SheetContent, SheetTrigger } from "@/app/components/ui/sheet";
+import Link from "next/link";
 import SideMenu from "./side-menu";
 
 const Header = () => {
   return (
     <Card>
       <CardContent className="p-5 justify-between items-center flex flex-row">
-        <Image src="/logo.svg" alt="FSW Barber" height={18} width={120} />
+        <Link href="/">
+          <Image src="/logo.svg" alt="FSW Barber" height={18} width={120} />
+        </Link>
 
         <Sheet>
           <SheetTrigger asChild>
