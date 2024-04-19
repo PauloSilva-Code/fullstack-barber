232695586ff5a3f959b683fc6ce361b9463f9fb1
Paulo Silva <paulowrsilva@gmail.com>
2024-04-19 15:19:21 -0300

    feat: ensure booking can be canceled

--- app/(home)/_components/booking-item.tsx
+++ app/(home)/_components/booking-item.tsx
@@ -1,13 +1,30 @@
+"use client";
+
+import { cancelBooking } from "@/app/_actions/cancel-booking";
 import {
   Avatar,
   AvatarFallback,
   AvatarImage,
 } from "@/app/components/ui/avatar";
 import { Badge } from "@/app/components/ui/badge";
+import { Button } from "@/app/components/ui/button";
 import { Card, CardContent } from "@/app/components/ui/card";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/app/components/ui/sheet";
 import { Prisma } from "@prisma/client";
-import { format, isPast } from "date-fns";
+import { format, isFuture } from "date-fns";
 import { ptBR } from "date-fns/locale";
+import { Loader } from "lucide-react";
+import Image from "next/image";
+import { useState } from "react";
+import { toast } from "sonner";
 
 interface BookingItemProps {
   booking: Prisma.BookingGetPayload<{
@@ -19,42 +36,151 @@ interface BookingItemProps {
 }
 
 const BookingItem = ({ booking }: BookingItemProps) => {
+  const [isDeleteLoading, setIsDeleteLoading] = useState(false);
+  const isBookingConfirmed = isFuture(booking.date);
+
+  const handleCancelClick = async () => {
+    setIsDeleteLoading(true);
+
+    try {
+      await cancelBooking(booking.id);
+
+      toast("Reserva cancelada com sucesso!");
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsDeleteLoading(false);
+    }
+  };
+
   return (
-    <Card className="min-w-full">
-      <CardContent className="py-0 flex px-0">
-        <div className="flex flex-col gap-2 py-5 pl-5 flex-[3]">
+    <Sheet>
+      <SheetTrigger asChild>
+        <Card className="min-w-full">
+          <CardContent className="py-0 flex px-0">
+            <div className="flex flex-col gap-2 py-5 pl-5 flex-[3]">
+              <Badge
+                // Passamos as cores bg-[] hover:bg-[] para raiz do componente "Badge".
+                variant={isBookingConfirmed ? "default" : "secondary"}
+                className="w-fit"
+              >
+                {isBookingConfirmed ? "Confirmado" : "Finalizado"}
+              </Badge>
+              <h2 className="font-bold">{booking.service.name}</h2>
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src={booking.service.imageUrl} />
+                  <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+                <h3 className="text-sm">{booking.barbershop.name}</h3>
+              </div>
+            </div>
+            <div className="flex flex-col items-center justify-center flex-1 border-l border-solid border-secondary">
+              <p className="text-sm capitalize">
+                {format(booking.date, "MMMM", {
+                  locale: ptBR,
+                })}
+              </p>
+              <p className="text-2xl">{format(booking.date, "dd")}</p>
+              <p className="text-sm">{format(booking.date, "hh:mm")}</p>
+            </div>
+          </CardContent>
+        </Card>
+      </SheetTrigger>
+
+      <SheetContent className="px-0">
+        <SheetHeader className="px-5 text-left pb-6 border-b border-solid border-secondary">
+          <SheetTitle>Informações da Reserva</SheetTitle>
+        </SheetHeader>
+
+        <div className="px-5">
+          <div className="relative h-[180px] w-full mt-6">
+            <Image
+              src="/barbershop-map.png"
+              alt={booking.barbershop.name}
+              fill
+            />
+
+            <div className="w-full absolute bottom-4 left-0 px-5">
+              <Card>
+                <CardContent className="p-3 flex gap-2">
+                  <Avatar>
+                    <AvatarImage src={booking.barbershop.imageUrl} />
+                  </Avatar>
+
+                  <div>
+                    <h2 className="font-bold">{booking.barbershop.name}</h2>
+                    <h3 className="text-xs overflow-hidden text-nowrap text-ellipsis">
+                      {booking.barbershop.address}
+                    </h3>
+                  </div>
+                </CardContent>
+              </Card>
+            </div>
+          </div>
+
           <Badge
-            // Passamos as cores bg-[] hover:bg-[] para raiz do componente "Badge".
-            variant={isPast(booking.date) ? "secondary" : "default"}
-            className="w-fit"
+            variant={isBookingConfirmed ? "default" : "secondary"}
+            className="w-fit mt-3 my-3"
           >
-            {isPast(booking.date) ? "Finalizado" : "Confirmado"}
+            {isBookingConfirmed ? "Confirmado" : "Finalizado"}
           </Badge>
 
-          <h2 className="font-bold">{booking.service.name}</h2>
+          <Card>
+            <CardContent className="p-3 gap-3 flex flex-col">
+              <div className="flex justify-between">
+                <h2 className="font-bold">{booking.barbershop.name}</h2>
+                <h3 className="font-bold text-sm">
+                  {" "}
+                  {Intl.NumberFormat("pt-BR", {
+                    style: "currency",
+                    currency: "BRL",
+                  }).format(Number(booking.service.price || 0.0))}
+                </h3>
+              </div>
 
-          <div className="flex items-center gap-2">
-            <Avatar className="h-6 w-6">
-              <AvatarImage src={booking.service.imageUrl} />
+              <div className="flex justify-between">
+                <h3 className="text-gray-400">Data</h3>
+                <h4 className="text-sm">
+                  {format(booking.date, "dd 'de' MMMM", {
+                    locale: ptBR,
+                  })}
+                </h4>
+              </div>
 
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
+              <div className="flex justify-between">
+                <h3 className="text-gray-400">Horário</h3>
+                <h4 className="text-sm">{format(booking.date, "hh:mm")}</h4>
+              </div>
 
-            <h3 className="text-sm">{booking.barbershop.name}</h3>
-          </div>
+              <div className="flex justify-between">
+                <h3 className="text-gray-400">Barbearia</h3>
+                <h4 className="text-sm">{booking.barbershop.name}</h4>
+              </div>
+            </CardContent>
+          </Card>
+
+          <SheetFooter className="flex-row gap-3 mt-6">
+            <SheetClose asChild>
+              <Button className="w-full" variant="secondary">
+                Voltar
+              </Button>
+            </SheetClose>
+            <Button
+              className="w-full"
+              variant="destructive"
+              disabled={!isBookingConfirmed || isDeleteLoading}
+              onClick={handleCancelClick}
+            >
+              {isDeleteLoading && (
+                <Loader className="mr-2 h-4 w-4 animate-spin" />
+              )}
+              Cancelar reserva
+            </Button>
+          </SheetFooter>
         </div>
-
-        <div className="flex flex-col items-center justify-center flex-1 border-l border-solid border-secondary">
-          <p className="text-sm capitalize">
-            {format(booking.date, "MMMM", {
-              locale: ptBR,
-            })}
-          </p>
-          <p className="text-2xl">{format(booking.date, "dd")}</p>
-          <p className="text-sm">{format(booking.date, "hh:mm")}</p>
-        </div>
-      </CardContent>
-    </Card>
+      </SheetContent>
+    </Sheet>
   );
 };
 
